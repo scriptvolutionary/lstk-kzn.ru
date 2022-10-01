1c6db305e275685ffbe0d362c1ee31e52ecd3927
scriptvolutionary <neckpuntop@gmail.com>
2022-10-01 11:29:32 +0300

    init

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	tabWidth: 4,
+	printWidth: 160,
+
+	semi: false,
+	useTabs: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+	bracketSameLine: false,
+	singleAttributePerLine: false,
+
+	quoteProps: 'as-needed',
+	arrowParens: 'avoid',
+	proseWrap: 'always',
+	trailingComma: 'none'
+}
